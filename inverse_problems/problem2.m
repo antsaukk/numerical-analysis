3a03cda71ccbba4d955e52d9ddc706eb1b20450a
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2022-05-06 20:15:13 +0300

    Update problem2.m

--- inverse_problems/problem2.m
+++ inverse_problems/problem2.m
@@ -12,28 +12,16 @@ U           = load("assignment2.mat");
 Y           = U.Y;
 w           = U.w;
 
-A = zeros(N, N);
-tic
-for k = 1:N
-    for i = 1:axis_length
-        for j = 1:axis_length
-            x   = [x1(i) x2(j) 0];
-            y   = Y(:,k);
-            ind = index(j)+index(i)*axis_length+1;
-            A(k,ind) = 1/norm(x - y);
-        end
-    end
-end
-A = 1/N * A;
-toc
+%A           = Grid1(N, axis_length, x1, x2, Y, index);
+A           = Grid2(N, axis_length, x1, x2, Y, index);
 
 z = zeros(size(w));                                                         % estimate from alternating algorithm
 gamma = 0.1;                                                                % estimate for stdev
 
-sigma = 0.005; % standard deviation in of noise in the data
-eps = sigma * sqrt(N); % Morozov criteria 
+sigma = 0.005;                                                              % standard deviation in of noise in the data
+eps = sigma * sqrt(N);                                                      % Morozov criteria 
 
-Aw = A'*w; % precompute constant terms 
+Aw = A'*w;                                                                  % precompute constant terms 
 %% CGA
 
 pcg = zeros(N, 1);
@@ -43,31 +31,28 @@ k = 1;
 % first step
 r = Aw - A'*(A*pcg);
 s = r;
-%res = norm(A*pcg - w);
-res = norm(Aw - A'*(A*pcg));
-res
+res = norm(A*pcg - w);
 residuals_cg(k) = res;
 
-run = res > eps; % run criteria: until Morozov is satisfied
+run = res > eps;                                                            % run criteria: until Morozov is satisfied
 
 tic
 while(run)
-    AAs = A'*(A*s); % precompute to speed-up
+    AAs             = A'*(A*s);                                             % precompute to speed-up
         
-    % Conjugate Gradient Algorithm
-    alpha = norm(r)^2/(s'*AAs);
-    pcg = pcg + alpha*s;
-    rk = r - alpha*AAs;
-    beta = (norm(rk)/norm(r))^2;
-    s = rk + beta*s;
-    k = k + 1;
+                                                                            % Conjugate Gradient Algorithm
+    alpha           = norm(r)^2/(s'*AAs);
+    pcg             = pcg + alpha*s;
+    rk              = r - alpha*AAs;
+    beta            = (norm(rk)/norm(r))^2;
+    s               = rk + beta*s;
+    k               = k + 1;
     
-    r = rk; % change of variables: rk=r_k+1
+    r               = rk;                                                   % change of variables: rk=r_k+1
         
-    %res = norm(A*pcg - w); % record residuals
-    res = norm(Aw - A'*(A*pcg));
+    res             = norm(A*pcg - w);                                      % record residuals
     residuals_cg(k) = res;
-    res
+
     run = res > eps; % run criteria
 end
 toc
@@ -87,7 +72,7 @@ gammas(1) = gamma;
 run = norm(A*z - w) > eps && abs(sigma-gamma) > 0.001; %wrong
 norm(A*z - w)
 tic
-while(run)
+while(kk < 2) % run
     % find z
     delta       = sigma^2/gamma^2;
     K           = [A; sqrt(delta) * eye(N)];
@@ -96,9 +81,8 @@ while(run)
 
     % find gamma
     gamma       = sqrt(2/N) * sigma * norm(z);
-    gamma
-    run         = norm(A*z - w) > eps && abs(sigma-gamma) > 0.001; %wrong
-    norm(A*z-w)
+    run         = norm(A*z - w) > eps; %wrong
+    norm(A*z-w)^2
     kk          = kk + 1;
     gammas(kk)  = gamma;
 end
@@ -119,3 +103,49 @@ set(gca,'YDir','normal');
 axis square
 caption = sprintf('Reconstruction via statistical inversion with k %d', kk-1);
 title(caption, 'FontSize', 14);
+%% Function for generation Grid1
+function A = Grid1(N, axis_length, x1, x2, Y, index)
+    A = zeros(N, N);
+    tic
+    for k = 1:N
+        for i = 1:axis_length
+            for j = 1:axis_length
+                x   = [x1(i) x2(j) 0];
+                y   = Y(:,k);
+                ind = j+index(i)*axis_length;
+                A(k,ind) = 1/norm(x - y);
+            end
+        end
+    end
+    A = 1/N * A;
+    toc
+end
+%% Function for generation Grid2
+function A = Grid2(N, axis_length, x1, x2, Y, index)
+    tic
+    %grid = [];
+    grid = zeros(3, N);
+
+    for i = 1:axis_length
+        for j = 1:axis_length
+            point                           = [1/80 + index(i)/axis_length
+                                               1/80 + index(j)/axis_length 
+                                               0];
+            %size(point)
+            %j + index(i)*axis_length
+            grid(:, j+index(i)*axis_length) = point;
+            %grid = [grid, point];
+        end
+    end
+    
+    A = zeros(N, N);
+
+    for k = 1:N
+        for l = 1:N
+            A(k,l) = 1/norm(grid(:,l) - Y(:, k));
+        end
+    end
+    A = 1/N * A;
+
+    toc
+end
