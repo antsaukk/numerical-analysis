29fe4c234176585f1a9bd2a3d05c68d2b646004c
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2022-05-06 21:47:08 +0300

    Update problem2.m

--- inverse_problems/problem2.m
+++ inverse_problems/problem2.m
@@ -1,44 +1,44 @@
 clear all;
 close all; 
 %% Preallocations
-index       = @(ind) ind-1;
+index       = @(ind) ind-1;                                                 % setting helpers functions
 
-N           = 1600;
+U           = load("assignment2.mat");                                      % loading data from the file
+Y           = U.Y;
+w           = U.w;
+
+N           = length(w);                                                    % fetching and setting the parameters
 axis_length = sqrt(N);
 h           = 1/axis_length;
 x1          = linspace(0, 1-h, axis_length);
 x2          = linspace(0, 1-h, axis_length);
-U           = load("assignment2.mat"); 
-Y           = U.Y;
-w           = U.w;
 
 %A           = Grid1(N, axis_length, x1, x2, Y, index);
 A           = Grid2(N, axis_length, x1, x2, Y, index);
 
-z = zeros(size(w));                                                         % estimate from alternating algorithm
-gamma = 0.1;                                                                % estimate for stdev
+z           = zeros(size(w));                                               % estimate from alternating algorithm
+gamma       = 0.1;                                                          % estimate for stdev
 
-sigma = 0.005;                                                              % standard deviation in of noise in the data
-eps = sigma * sqrt(N);                                                      % Morozov criteria 
+sigma       = 0.005;                                                        % standard deviation in of noise in the data
+eps         = sigma * sqrt(N);                                              % Morozov criteria 
 
 Aw = A'*w;                                                                  % precompute constant terms 
 %% CGA
 
-pcg = zeros(N, 1);
-residuals_cg = [];
-k = 1; 
+pcg             = zeros(N, 1);                                              % saving iterates of CGA
+residuals_cg    = [];                                                       % saving residuals of CGA
+k               = 1;                                                        % iterations counter
 
-% first step
-r = Aw - A'*(A*pcg);
-s = r;
-res = norm(A*pcg - w);
+r               = Aw - A'*(A*pcg);                                          % first step of CGA
+s               = r;
+res             = norm(A*pcg - w);
 residuals_cg(k) = res;
 
-run = res > eps;                                                            % run criteria: until Morozov is satisfied
+run             = res > eps;                                                % run criteria: until Morozov is satisfied
 
 tic
 while(run)
-    AAs             = A'*(A*s);                                             % precompute to speed-up
+    AAs             = A'*(A*s);                                             % precompute iteration-constant terms to speed-up
         
                                                                             % Conjugate Gradient Algorithm
     alpha           = norm(r)^2/(s'*AAs);
@@ -53,38 +53,31 @@ while(run)
     res             = norm(A*pcg - w);                                      % record residuals
     residuals_cg(k) = res;
 
-    run = res > eps; % run criteria
+    run             = res > eps;                                            % check run criteria
 end
 toc
 
-Pcg = reshape(pcg, 40, 40);
-figure(1)
-imagesc([0,1], [0,1], Pcg);
-set(gca,'YDir','normal');
-axis square
-caption = sprintf('Reconstruction with k %d', k-1);
-title(caption, 'FontSize', 14);
-%% Alternating algorithm to find MAP
-kk = 1;
-gammas = [];
-gammas(1) = gamma;
+visualize2d(pcg, axis_length, 1, k, 'Reconstruction by CGA with k %d');     % plot reconstruction
+%% Alternating algorithm to find Z_MAP and gamma_MAP
+kk          = 1;                                                            % iterations counter
+gammas      = [];                                                           % recording residuals of gamma
+gammas(1)   = gamma;                                                        % initial value of gamma
 
-run = norm(A*z - w) > eps && abs(sigma-gamma) > 0.001; %wrong
+run = norm(A*z - w) > eps; %wrong                                           % stopping according to Morozov criteria                                      
 norm(A*z - w)
+
 tic
-while(kk < 2) % run
-    % find z
-    delta       = sigma^2/gamma^2;
+while(run) % run
+    delta       = sigma^2/gamma^2;                                          % compute minimizer for Z_map using Tikhonov functional
     K           = [A; sqrt(delta) * eye(N)];
     v           = [w; zeros(N, 1)];
     z           = K\v;
 
-    % find gamma
-    gamma       = sqrt(2/N) * sigma * norm(z);
-    run         = norm(A*z - w) > eps; %wrong
-    norm(A*z-w)^2
+    gamma       = norm(z)/sqrt(N);                                          % compute gamma from T'(z,gamma)=0
+    run         = norm(A*z - w) > eps;
+    
     kk          = kk + 1;
-    gammas(kk)  = gamma;
+    gammas(kk)  = gamma;                                                    % record gamma
 end
 toc
 
@@ -96,13 +89,7 @@ legend('Prior stdev')
 grid on
 hold off
 
-Z = reshape(z, 40, 40);
-figure(3)
-imagesc([0,1], [0,1], Z);
-set(gca,'YDir','normal');
-axis square
-caption = sprintf('Reconstruction via statistical inversion with k %d', kk-1);
-title(caption, 'FontSize', 14);
+visualize2d(z, axis_length, 3, kk, 'Reconstruction via statistical inversion with k %d');
 %% Function for generation Grid1
 function A = Grid1(N, axis_length, x1, x2, Y, index)
     A = zeros(N, N);
@@ -110,9 +97,9 @@ function A = Grid1(N, axis_length, x1, x2, Y, index)
     for k = 1:N
         for i = 1:axis_length
             for j = 1:axis_length
-                x   = [x1(i) x2(j) 0];
-                y   = Y(:,k);
-                ind = j+index(i)*axis_length;
+                x        = [x1(i) x2(j) 0];
+                y        = Y(:,k);
+                ind      = j+index(i)*axis_length;
                 A(k,ind) = 1/norm(x - y);
             end
         end
@@ -131,8 +118,6 @@ function A = Grid2(N, axis_length, x1, x2, Y, index)
             point                           = [1/80 + index(i)/axis_length
                                                1/80 + index(j)/axis_length 
                                                0];
-            %size(point)
-            %j + index(i)*axis_length
             grid(:, j+index(i)*axis_length) = point;
             %grid = [grid, point];
         end
@@ -148,4 +133,17 @@ function A = Grid2(N, axis_length, x1, x2, Y, index)
     A = 1/N * A;
 
     toc
+end
+%% Visualization function
+function visualize2d(pix, n, k, iter, str)
+    Pix = reshape(pix, n, n);
+
+    figure(k)
+    imagesc([0,1], [0,1], Pix);
+    set(gca,'YDir','normal');
+    axis square
+    title(sprintf('Reconstruction via statistical inversion with k %d', ...
+            iter-1), ...
+            'FontSize', ...
+            14);
 end
