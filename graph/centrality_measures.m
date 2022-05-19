faeb614bb0d7fcab0d783f3914f8293a0f3a2fde
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2022-05-19 17:32:09 +0300

    Update centrality_measures.m

--- graph/centrality_measures.m
+++ graph/centrality_measures.m
@@ -1,9 +1,11 @@
 clear all;
 close all;
 %% Preallocs
-Data            = load("mycielskian8.mat");
+Data             = load("mycielskian8.mat");
 AdjacencyMatrix0 = Data.Problem.A;
-Graph           = graph(AdjacencyMatrix0);
+Graph            = graph(AdjacencyMatrix0);
+
+figure(1)
 plot(Graph, 'NodeColor', 'k')
 
 [ny0, nx0]    = size(AdjacencyMatrix0);
@@ -11,24 +13,44 @@ plot(Graph, 'NodeColor', 'k')
 %% Experiment: Katz centrality for each node as function of alpha
 grid_size_K = 9;    
 alphas      = linspace(0, 1/max(abs(diag(L0))), grid_size_K);
-alphas      = alphas(2:end);
+alphas      = alphas(2:end-1); %tai end
 
 kvcent    = zeros(ny0, length(alphas));
 
 for index = 1:length(alphas)
     kvcent(:, index) = KatzCentrality(AdjacencyMatrix0, alphas(index));
+end 
+
+figure(2)
+for i = 1:191
+    %index = i * 10 + 1;
+    plot(alphas, kvcent(i,:), '--o')
+    hold on
 end
+%ax.XTickLabel = string(alphas);
+grid on
+hold off
 %% Experiment: NBT centrality for each node as function of t
 grid_size_NBT = 9;    
 ts            = linspace(0, 1, grid_size_NBT);
-ts            = ts(2:end);
+ts            = ts(2:end-1); %tai end
 
 nbtwcent      = zeros(ny0, length(ts));
 
 for index = 1:length(ts)
     nbtwcent(:, index) = NBTCentrality(AdjacencyMatrix0, ts(index));
 end
-%% Compute Katz-Centrality with given Adjacency matrix and alpha parameter
+
+figure(3)
+for i = 1:191
+    %index = i * 10 + 1;
+    plot(ts, nbtwcent(i,:), '--o')
+    hold on
+end
+%ax.XTickLabel = string(ts);
+grid on
+hold off
+%% Katz-Centrality with given Adjacency matrix and alpha parameter
 function katz_cen = KatzCentrality(A, alpha)
     [ny nx]  = size(A);
     assert(nx == ny, 'Matrix is non-square'); 
@@ -36,8 +58,9 @@ function katz_cen = KatzCentrality(A, alpha)
     I        = eye(ny, nx);
     e        = ones(ny, 1);
     katz_cen = (I - alpha*A)\e;
+    %katz_cen = inv((I - alpha*A) - I)*e;
 end
-%% Compute NBT centrality with given Adjacency matrix and alpha parameter
+%% NBT centrality with given Adjacency matrix and alpha parameter
 function nbt_cen = NBTCentrality(A, t)
     [ny nx]  = size(A);
     assert(nx == ny, 'Matrix is non-square');
