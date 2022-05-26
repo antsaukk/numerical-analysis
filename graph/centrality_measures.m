420af53e5884390be9a53a176976096ad479a951
Anton Saukkonen <63663359+antsaukk@users.noreply.github.com>
2022-05-26 20:56:43 +0300

    Update centrality_measures.m

--- graph/centrality_measures.m
+++ graph/centrality_measures.m
@@ -1,7 +1,9 @@
 clear all;
 close all;
 %% Preallocs
-Data             = load("mycielskian8.mat");
+%Data             = load("mycielskian8.mat");
+Data             = load("GD96_c.mat");
+%Data             = load("delaunay_n12.mat");
 AdjacencyMatrix0 = Data.Problem.A;
 Graph            = graph(AdjacencyMatrix0);
 
@@ -13,7 +15,7 @@ plot(Graph, 'NodeColor', 'k')
 %% Experiment: Katz centrality for each node as function of alpha
 grid_size_K = 9;    
 alphas      = linspace(0, 1/max(abs(diag(L0))), grid_size_K);
-alphas      = alphas(2:end-1); %tai end
+alphas      = alphas(2:end-1);
 
 kvcent    = zeros(ny0, length(alphas));
 
@@ -22,29 +24,39 @@ for index = 1:length(alphas)
 end 
 
 figure(2)
-for i = 1:191
-    %index = i * 10 + 1;
-    plot(alphas, kvcent(i,:), '--o')
+for i = 1:length(AdjacencyMatrix0)
+    plot(alphas, kvcent(i,:))
     hold on
 end
 %ax.XTickLabel = string(alphas);
 grid on
 hold off
 %% Experiment: NBT centrality for each node as function of t
-grid_size_NBT = 9;    
-ts            = linspace(0, 1, grid_size_NBT);
-ts            = ts(2:end-1); %tai end
+grid_size_NBT = 100;
+
+[ny nx]  = size(AdjacencyMatrix0);
+assert(nx == ny, 'Matrix is non-square');
+    
+I        = speye(ny, nx);
+e        = ones(ny, 1);
+D        = diag(sum(AdjacencyMatrix0));
+Lam      = polyeig(I, AdjacencyMatrix0, (D-I));
+
+min(abs(Lam))
+
+ts            = linspace(0, min(abs(Lam)), grid_size_NBT);
+ts            = ts(2:end-1);
 
 nbtwcent      = zeros(ny0, length(ts));
 
 for index = 1:length(ts)
-    nbtwcent(:, index) = NBTCentrality(AdjacencyMatrix0, ts(index));
+    nbtwcent(:, index) = NBTCentrality(I, D, AdjacencyMatrix0, e, ts(index));
 end
 
 figure(3)
-for i = 1:191
+for i = 1:length(AdjacencyMatrix0)
     %index = i * 10 + 1;
-    plot(ts, nbtwcent(i,:), '--o')
+    plot(ts, nbtwcent(i,:))
     hold on
 end
 %ax.XTickLabel = string(ts);
@@ -58,16 +70,9 @@ function katz_cen = KatzCentrality(A, alpha)
     I        = eye(ny, nx);
     e        = ones(ny, 1);
     katz_cen = (I - alpha*A)\e;
-    %katz_cen = inv((I - alpha*A) - I)*e;
 end
-%% NBT centrality with given Adjacency matrix and alpha parameter
-function nbt_cen = NBTCentrality(A, t)
-    [ny nx]  = size(A);
-    assert(nx == ny, 'Matrix is non-square');
-    
-    I        = eye(ny, nx);
-    e        = ones(ny, 1);
-    D        = diag(sum(A));
-    Mt       = I - A*t + (D - I)*t^2;
+%% NBT centrality with given Adjacency matrix and t parameter
+function nbt_cen = NBTCentrality(I, D, A, e, t)
+    Mt       = I - t*A + (D - I) * t^2;
     nbt_cen  = (1 - t^2) * (Mt\e);
 end
